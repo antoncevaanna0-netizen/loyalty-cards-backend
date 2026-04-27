53561b3c95fc839caacdcf37b7861128b8b6af1b
antoncevaanna0-netizen <antoncevaanna0@gmail.com>
2026-04-28 02:46:48 +0600

    Отключен helmet для теста CORS

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.set('trust proxy', 1);
 
 // Базовая защита заголовков
-app.use(helmet());
+//app.use(helmet());
 
 // Ограничение запросов
 const limiter = rateLimit({
@@ -38,7 +38,7 @@ const authLimiter = rateLimit({
 app.use(limiter);
 
 app.use(cors({
-    origin: '*',
+    origin: process.env.CORS_ORIGIN || '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
