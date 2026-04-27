4002f56cc50e5edb05185eec282b991ff14de2ef
Yliana <antoncevaanna0@gmai.com>
2026-04-28 00:48:50 +0600

    Использование DATABASE_URL для Railway

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,11 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const connectionString = process.env.DATABASE_URL || 
+    `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+    connectionString,
     ssl: {
         rejectUnauthorized: false
     }
